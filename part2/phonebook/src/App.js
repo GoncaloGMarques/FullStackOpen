d733bf41d1bc71a7be76c4e2d5d2bfa769f34793
GoncaloGMarques <gongamarques96@gmail.com>
2023-03-31 22:37:12 +0100

    Update - 2.11 Phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,27 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from "axios";
 
 import Filter from './Filter'
 import PersonForm from './PersonForm'
 import Persons from './Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchStr, setSearchStr] = useState('')
 
   function onCLickAddPerson(event) {
     event.preventDefault();
-    if (persons.some((person) => person.name == newName )) {
+    if (persons.some((person) => person.name == newName)) {
       alert(`${newName} is already added to phonebook`);
       return;
     }
-    if (persons.some((person) => person.number == newNumber )) {
+    if (persons.some((person) => person.number == newNumber)) {
       let foundPhone = persons.filter((element) => element.number == newNumber)[0]
       alert(`${newNumber} is already added to phonebook as ${foundPhone.name}`);
       return;
@@ -51,17 +47,25 @@ const App = () => {
       changeCallback: onChangeNumber
     }
   ]
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((personsData) => {
+      console.log(personsData);
+      setPersons(personsData?.data);
+    });
+  }, []);
 
   const numbersToShow = searchStr != '' ? persons.filter((person) => person.name.toLowerCase().includes(searchStr.toLowerCase())) : persons;
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter onChangeCallback={onChangeSearchParam}/>
+      <Filter onChangeCallback={onChangeSearchParam} />
       <h2>Add a new</h2>
       <PersonForm controls={controls} submitCallback={onCLickAddPerson} />
       <h2>Numbers</h2>
-      <Persons listOfNumbers={numbersToShow} />
+      {numbersToShow.length != 0 &&
+        <Persons listOfNumbers={numbersToShow} />
+      }
     </div>
   )
 }
